521e0ebdfb52899fc82a516b6bc5cacac00da91f
Artur Kosmovskij <kosmos146139@gmail.com>
2024-02-06 11:29:37 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        character = Character.toLowerCase(character);
+        if (!((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z') )) {System.out.println("wrong alphabet!");}
+        else if (character == 'a' || character == 'o' || character == 'e' || character == 'u' || character == 'i' || character == 'y') {System.out.println("Vowel");}
+        else {System.out.println("Consonant");}
     }
 }
